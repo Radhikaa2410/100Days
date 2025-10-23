61824f42a49c3746e2805fa75face72a5e0b04d6
Radhikaa2410 <radhyashmann.0607@gmail.com>
2025-10-23 23:14:15 +0530

    100DayOfCode

--- Day40/Day40Q1.c
+++ Day40/Day40Q1.c
@@ -7,7 +7,7 @@ int main()
 
     int a[n][n];
     printf("Enter %d elements:\n", n * n);
-    for(int i = 0; i < n; i++) 
+    for(int i = 0; i<n; i++) 
     {
         for(int j = 0; j < n; j++) 
         {
