054be88c11100470aceaedce7f3a4efc97093cd0
Radhikaa2410 <radhyashmann.0607@gmail.com>
2025-11-18 21:34:19 +0530

    100Days

--- Day7/Day7Q1.c
+++ Day7/Day7Q1.c
@@ -4,7 +4,7 @@
 int main()
  {
     int year;
-    printf("Enter a year: ");
+    printf("Enter a year:");
     scanf("%d", &year);
     if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) 
     {
