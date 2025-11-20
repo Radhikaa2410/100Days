f4fe0182ee7b356bae12cc4ea2e6df64aa38d99c
Radhikaa2410 <radhyashmann.0607@gmail.com>
2025-11-20 22:03:01 +0530

    100Days

--- Day4/Day4Q1.c
+++ Day4/Day4Q1.c
@@ -5,12 +5,12 @@ int main()
     int a,b;
     printf("Enter numbers\n");
     scanf("%d%d",&a,&b);
-    printf("Values before swapping \n");
+    printf("Values before swapping: \n");
     printf("a=%d b=%d",a,b);
     a=a+b;
     b=a-b;
     a=a-b;
-    printf("Values after swapping \n");
+    printf("Values after swapping: \n");
     printf("a=%d b=%d",a,b);
     return 0;
 }
